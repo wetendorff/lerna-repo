2b050d532662b1b296733a6b601d9a4f8fe1d193
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-07 11:03:09 +0200

    feat: add secondary parameter

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -3,6 +3,6 @@
 module.exports = testpkg01;
 
 // Added comment
-function testpkg01(param) {
+function testpkg01(param, param2) {
   console.log("Hello from test package 1: " + param); // Added more comments
 }
