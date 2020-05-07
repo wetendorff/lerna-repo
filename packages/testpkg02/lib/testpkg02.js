f43fd8ee6c842b649ebc46843534801d5b8744f1
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-07 09:46:05 +0200

    feat: add parameter to testpkg02 function

--- packages/testpkg02/lib/testpkg02.js
+++ packages/testpkg02/lib/testpkg02.js
@@ -2,6 +2,6 @@
 
 module.exports = testpkg02;
 
-function testpkg02() {
-  console.log("Hello from test package 2");
+function testpkg02(param) {
+  console.log("Hello from test package 2:" + param);
 }
