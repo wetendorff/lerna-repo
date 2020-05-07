0307c9b6ed6ffce34741056e6eae6004caf626a6
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-07 12:06:22 +0200

    feat: add sum function

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -2,6 +2,10 @@
 
 module.exports = testpkg01;
 
+function sum(a, b) {
+  return a + b;
+}
+
 // Added comment
 function testpkg01(param, param2, param3) {
   console.log("Hello from test package 1: " + param); // Added more comments
