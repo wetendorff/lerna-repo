88664a98caf3235629a2f4828587e67d0700880e
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-07 09:49:56 +0200

    feat: add param to testpkg01 function

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -2,6 +2,6 @@
 
 module.exports = testpkg01;
 
-function testpkg01() {
-  console.log("Hello from test package 1 - new change again");
+function testpkg01(param) {
+  console.log("Hello from test package 1: " + param);
 }
