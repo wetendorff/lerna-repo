4a4ca03cb30e9636e6cac3580da4afad2753b34e
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-11 13:04:49 +0200

    feat: remove unused params

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -7,6 +7,6 @@ function sum(a, b) {
 }
 
 // Added comment
-function testpkg01(param, param2, param3) {
+function testpkg01(param) {
   console.log("Hello from test package 1: " + param); // Added more comments
 }
