638fbee1a858e27fdb16f82f10f76977378a5daa
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-11 15:39:25 +0200

    feat: add new function

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -10,3 +10,7 @@ function sum(a, b) {
 function testpkg01(param) {
   console.log("Hello from test package 1: " + param); // Added more comments
 }
+
+function functwo() {
+  console.log("New function");
+}
