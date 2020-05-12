0d1b595cde2c845947d5a28d4b2057f2ebf70158
Lars Wetendorff Nielsen <lawn@berlingskemedia.dk>
2020-05-12 08:41:21 +0200

    fix: remove another function

--- packages/testpkg01/lib/testpkg01.js
+++ packages/testpkg01/lib/testpkg01.js
@@ -2,10 +2,6 @@
 
 module.exports = testpkg01;
 
-function sum(a, b) {
-  return a + b;
-}
-
 // Added comment
 function testpkg01(param) {
   console.log("Hello from test package 1: " + param); // Added more comments
